af6f188571ab1504a4cdc2cd04cfcb39846633ac
rileybr <brandon.riley@ssfs.org>
2019-11-22 14:52:54 -0500

    Finds and compares the scores of two players playing a nine hole game and prints the totals, how they did compared to the par, how many holes the winner won by, and the hole with the most number of strokes used

---
+++ src/com/company/GolfMedium.java
@@ -0,0 +1,110 @@
+package com.company;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class GolfMedium {
+    public static int checkScore(int par, int using_score){
+        int score = 0;
+        String output = "";
+        if (par == using_score){
+            score += 0;
+        }
+        else if (par == (using_score + 1)){
+            output += "birdie\n";
+            score += -1;
+        }
+        else if (par == (using_score + 2)){
+            output += "eagle\n";
+            score += -2;
+        }
+        else if (par == (using_score - 1)){
+            output += "bogey\n";
+            score += 1;
+        }
+        else if (par == (using_score - 2)){
+            output += "double bogey\n";
+            score += 2;
+        }
+        return score;
+    }
+
+    public static String findScore(){
+        File file = new File("C:\\Users\\rileybr\\IdeaProjects\\UnitFour\\src\\com\\company\\golf_medium_scores.txt");
+        Scanner scanner = null;
+        try {
+            scanner = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+
+        String output = "";
+        String temp_output = "";
+        int a_score = 0;
+        int b_score = 0;
+        int a_score_total = 0;
+        int b_score_total = 0;
+        int temp_1 = 0;
+        int temp_2= 0;
+
+        for (int temp = 0; temp < 9; temp++){
+            String line = scanner.nextLine();
+            String first_temp = line.substring(0, 1);
+            String second_temp = line.substring(2,3);
+            String third_temp = line.substring(4,5);
+            int par = Integer.parseInt(first_temp);
+            int player_a_score = Integer.parseInt(second_temp);
+            int player_b_score = Integer.parseInt(third_temp);
+
+            a_score_total += player_a_score;
+            b_score_total += player_b_score;
+            a_score += checkScore(par, player_a_score);
+            b_score += checkScore(par, player_b_score);
+
+            temp_1 = player_a_score + player_b_score;
+
+            if (temp_1 > temp_2){
+                temp_2 = temp_1;
+                temp_output = (temp + 1) + " " + temp_2;
+            }
+
+        }
+
+
+        String a_par = "";
+        if (a_score < 0){
+            a_par = (int) Math.abs(a_score) + " under par\n";
+        }
+        else {
+            a_par = a_score + " over par\n";
+        }
+        String b_par = "";
+        if (b_score < 0){
+            b_par = (int) Math.abs(b_score) + " under par\n";
+        }
+        else {
+            b_par = b_score + " over par\n";
+        }
+
+        if (a_score <= b_score){
+            output += a_score_total + " " + b_score_total+"\n";
+            output += a_par;
+            output += b_par;
+            output += b_score_total - a_score_total +"\n";
+            output += temp_output;
+        }
+        else {
+            output += b_score_total + " " + a_score_total+"\n";
+            output += b_par;
+            output += a_par;
+            output += a_score_total - b_score_total +"\n";
+            output += temp_output;
+        }
+        return output;
+    }
+    public static void main(String[] args){
+        System.out.println(findScore());
+    }
+}
